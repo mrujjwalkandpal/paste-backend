886ecf081043b8f64803c947a872a1cff183e14b
Ujjwal Kandpal <153886946+mrujjwalkandpal@users.noreply.github.com>
2026-03-31 19:35:07 +0530

    Remove unnecessary blank line in PasteBinApplication

--- src/main/java/com/ujjwal/paste_bin/PasteBinApplication.java
+++ src/main/java/com/ujjwal/paste_bin/PasteBinApplication.java
@@ -1,5 +1,5 @@
 package com.ujjwal.paste_bin;
-
+ 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
