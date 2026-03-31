2c1c418a3660e8eb203c289c4a291d0aff313502
Ujjwal Kandpal <153886946+mrujjwalkandpal@users.noreply.github.com>
2026-03-31 19:34:31 +0530

    Enable scheduling in PasteBinApplication

--- src/main/java/com/ujjwal/paste_bin/PasteBinApplication.java
+++ src/main/java/com/ujjwal/paste_bin/PasteBinApplication.java
@@ -2,7 +2,9 @@ package com.ujjwal.paste_bin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class PasteBinApplication {
 
