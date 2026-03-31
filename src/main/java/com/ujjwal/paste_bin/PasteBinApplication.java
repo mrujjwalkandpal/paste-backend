3e12d5b3f8f8149b93abcf8569f56c613df8b6f9
mrujjwalkandpal <kandpalujjwal24@gmail.com>
2026-03-31 08:50:59 +0530

    a

---
+++ src/main/java/com/ujjwal/paste_bin/PasteBinApplication.java
@@ -0,0 +1,13 @@
+package com.ujjwal.paste_bin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasteBinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasteBinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ujjwal/paste_bin/PasteBinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ujjwal.paste_bin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasteBinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
